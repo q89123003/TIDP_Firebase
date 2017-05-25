281660c3a7b26d3b18d8f2e1d89c3fb2402b11db
Chang Yu-Wei <mn228426@gmail.com>
2017-05-25 21:42:16 +0800

    test

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,8 @@ var functions = require('firebase-functions');
  exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
  });
+
+ exports.helloTest = functions.https.onRequest((request, response) => {
+  response.send("Test Test from Firebase!");
+ });
+

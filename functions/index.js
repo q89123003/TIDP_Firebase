d4ad6f0cd90705d310d6bcf4ba71cbf3c86ce5ec
q89123003 <b02901092@ntu.edu.tw>
2017-06-09 02:17:16 +0800

    post to database

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
-var functions = require('firebase-functions');
+'use strict';
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase)
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -14,5 +17,17 @@ var functions = require('firebase-functions');
  exports.gps = functions.https.onRequest((request, response) => {
  	var lat = request.body.latitude;
  	var lon = request.body.longitude;
+
  	response.send("Lat = " + lat);
+
+ 	var time = 100
+ 	var userID = 92
+
+ 	push_data = {userID: userID, time: time, latitude: lat, longitude: lon};
+  	// [START adminSdkPush]
+  	// Push the new message into the Realtime Database using the Firebase Admin SDK.
+  	admin.database().ref('/test').push(push_data).then(snapshot => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    		res.redirect(303, snapshot.ref);
+		});
  });
---
+++ http_request_test.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+url = 'https://us-central1-tidp-fusionnext.cloudfunctions.net/gps'
+data = {'latitude': 123, 'longitude': 444}
+headers = {'Content-type': 'application/json'}
+r = requests.post(url, data=json.dumps(data), headers=headers)
+
+print(r.text)

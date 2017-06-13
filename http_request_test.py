65675eafc93f4854d6adaba3bce0fcac423dddee
q89123003 <b02901092@ntu.edu.tw>
2017-06-13 21:13:54 +0800

    modify http test py

--- http_request_test.py
+++ http_request_test.py
@@ -1,13 +1,16 @@
 import requests
 import json
 
-url = 'https://us-central1-tidp-fusionnext.cloudfunctions.net/gps'
-data = {'latitude': 123, 'longitude': 444}
-headers = {'Content-type': 'application/json'}
-r = requests.post(url, data=json.dumps(data), headers=headers)
+testMode = 2
 
-print(r.text)
+if testMode == 1:
+	url = 'https://us-central1-tidp-fusionnext.cloudfunctions.net/gps'
+	data = {'latitude': 123, 'longitude': 444}
+	headers = {'Content-type': 'application/json'}
+	r = requests.post(url, data=json.dumps(data), headers=headers)
+	print(r.text)
 
-url = 'https://us-central1-tidp-fusionnext.cloudfunctions.net/queryTest'
-r = requests.get(url)
-print(r.text)
+if testMode == 2:
+	url = 'https://us-central1-tidp-fusionnext.cloudfunctions.net/queryTest'
+	r = requests.get(url)
+	print(r.text)
